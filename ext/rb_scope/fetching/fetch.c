dd11201fc6f0ab82aa1988fa715acf704ba29def
hugo benichi <hugo.benichi@m4x.org>
2013-03-18 17:45:16 +0900

    update to fetch C code

--- ext/rb_scope/fetching/fetch.c
+++ ext/rb_scope/fetching/fetch.c
@@ -1,34 +1,32 @@
 #include  <stdio.h>
 #include  <stdlib.h>
+
 #include  "fetch.h"
-#include 	"niScope.h" 
+#include  "niScope.h"
 
 
 #define   prompt(MSG, ...)    printf("RbScope::Fetching >> " MSG, __VA_ARGS__)
 
 
-  typedef   
-    struct niScope_wfmInfo 
-    wfm_info;
-    
-  
-DLL int fetch(
-    fetch_hlp* parameters
-  )
-{
-    
+typedef
+  struct niScope_wfmInfo
+  wfm_info;
+
+
+DLL int fetch(fetch_hlp* parameters) {
+
   unsigned int    session   = parameters-> session;
   char*           address   = parameters-> address;
   int             frm_siz   = parameters-> frm_siz;
   int             frm_tot   = parameters-> frm_tot;
   int             frm_chk   = parameters-> frm_chk;
   double          timeout   = parameters-> timeout;
-  
+
   char*           buf       = NULL;
   buffer_address  buf_get   = parameters-> buffer_get;
   void*           context   = parameters-> context;
   error_handler   err_clbck = parameters-> err_clbck;
-   
+
   #define err_check(ret_code)  do {\
                                   int err_check_local_err_code = ret_code;\
                                   if( err_check_local_err_code < 0)\
@@ -40,28 +38,62 @@ DLL int fetch(
                                     return -1;\
                                   }\
                                 } while (0)
-                                
-  wfm_info *frm_inf_p = malloc( sizeof(wfm_info) * 2 * frm_chk );
-  char* loc_buffer = malloc( 2 * frm_chk * frm_siz * sizeof(char)  ); 
 
-  int i, rez, records_done;  
-  
-  prompt("starting fetching on %s\n record length %i  |  record tot num %i  |  chunk %i\n", address, frm_siz, frm_tot, frm_chk);  
-  for (i = 0; i < frm_tot; i += frm_chk) 
-  { 
-    err_check( niScope_SetAttributeViInt32(session, VI_NULL, NISCOPE_ATTR_FETCH_RECORD_NUMBER, i) );
+  wfm_info *frm_inf_p = malloc( sizeof(wfm_info) * 2 * frm_chk );
+  char* loc_buffer = malloc( 2 * frm_chk * frm_siz * sizeof(char)  );
+
+  int i, rez, records_done;
+
+  prompt(
+    "fetching from %s\n  rec length %i  |  rec tot num %i  |  chunk %i\n",
+    address,
+    frm_siz,
+    frm_tot,
+    frm_chk
+  );
+
+  for (i = 0; i < frm_tot; i += frm_chk) {
+
+    err_check(
+      niScope_SetAttributeViInt32(
+        session,
+        VI_NULL,
+        NISCOPE_ATTR_FETCH_RECORD_NUMBER,
+        i
+      )
+    );
+
     if (buf_get && !(buf = buf_get(context)) ) buf = loc_buffer;
-    err_check( niScope_FetchBinary8(session, "0,1", timeout, frm_siz, buf, frm_inf_p) );
-    err_check( niScope_GetAttributeViInt32(session, VI_NULL, NISCOPE_ATTR_RECORDS_DONE, &records_done) );
-    prompt("%s, frames fetched #%i | measured #%i | in dev mem %i\n", address, i, records_done, records_done - i);
+
+    err_check(
+      niScope_FetchBinary8(session, "0,1", timeout, frm_siz, buf, frm_inf_p)
+    );
+
+    err_check(
+      niScope_GetAttributeViInt32(
+        session,
+        VI_NULL,
+        NISCOPE_ATTR_RECORDS_DONE,
+        &records_done
+      )
+    );
+
+    prompt(
+      "%s, frames fetched #%i | measured #%i | in dev mem %i\n",
+      address,
+      i,
+      records_done,
+      records_done - i
+    );
+
   }
-  
+
   free(loc_buffer);
-  
+
   return records_done;
-  
+
   #undef err_check
-  
+
 }
 
 
--- ext/rb_scope/fetching/fetch.h
+++ ext/rb_scope/fetching/fetch.h
@@ -1,36 +1,35 @@
-
-
 #ifndef   RB_SCOPE_FETCHING_HEADER
   #define   RB_SCOPE_FETCHING_HEADER
 
   #define 	DLL     extern __declspec(dllexport)
-    
-  typedef   
+
+  typedef
     struct fetch_helper
     fetch_hlp;
-    
+
   typedef
-    char*     (*buffer_address)(void*);
-    
+    char* (*buffer_address)(void*);
+
   typedef
-    void      (*error_handler)(int);
-  
-  struct 
-    fetch_helper
-    {
-	    char*				      address;        // physical address of the device
-      error_handler     err_clbck;      // ruby callback to manage errors
-      buffer_address    buffer_get;     // return the address of the next 
-      void*             context;        // context for the buffer_address functions
-      double            timeout;        // in sec; -1 = infinite     
-      int               frm_siz;        // numbr of data points per one frame
-      int               frm_tot;        // total number of frm to fetch, per channels all chans one acq
-      int               frm_chk;        // numbr of frames to read in one packet (set by the buffer size)  
-	    unsigned int	    session;        // (ViSession pointer id used by the niScope API)
-    };
-  
+    void  (*error_handler)(int);
+
+  struct fetch_helper {
+	  char*				address;    /* physical address of the device */
+      error_handler     err_clbck;  /* ruby callback to manage errors */
+      buffer_address    buffer_get; /* return the address of the next
+                                       buffer to hold device data */
+      void*             context;    /* client context for the buffer_address
+                                       functions */
+      double            timeout;    /* in sec; -1 = infinite */
+      int               frm_siz;    /* numberr of data points per frame */
+      int               frm_tot;    /* total number of frame to fetch,
+                                       per channels, for all chans, for 1 acq */
+      int               frm_chk;    /* numbr of frames to read in one packet
+                                       (set by the buffer size) */
+	  unsigned int	    session;    /* ViSession pointer id used by the
+                                       niScope API */
+  };
+
   DLL   int  fetch (fetch_hlp* parameters);
 
 #endif
-
-
